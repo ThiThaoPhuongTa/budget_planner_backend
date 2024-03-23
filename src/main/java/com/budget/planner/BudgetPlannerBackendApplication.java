3d527931d7ea051d4ad0fbaa64798afb72cbb959
ThiThaoPhuongTa <tathaophuong1905@gmail.com>
2024-03-23 21:25:15 +0700

    init spring boot project

---
+++ src/main/java/com/budget/planner/BudgetPlannerBackendApplication.java
@@ -0,0 +1,13 @@
+package com.budget.planner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BudgetPlannerBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BudgetPlannerBackendApplication.class, args);
+	}
+
+}
